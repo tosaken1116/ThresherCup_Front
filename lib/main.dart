846ae290577281a8c3367af27f1368bcc47f3873
Sea10wood <miyusea5@gmail.com>
2023-08-26 15:23:22 +0900

    add/#2_HomeSettingPage

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'SignupPage.dart';
 import 'ProfileExample.dart';
 import 'ProfileDetails.dart';
 import 'Profile.dart';
+import 'UserAllPage.dart';
+import 'HomeSetting.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,14 +15,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'ClosePass',
       initialRoute: '/',
       routes: {
         '/': (context) => LoginPage(),
         '/first': (context) => SignupPage(),
+        '/homesetting':(context) => HomeSetting(),
         '/second': (context) => ProfileExample(),
         '/third': (context) => ProfileDetails(),
         '/fourth':(context) => Profile(),
+        '/fifth':(context) => UserAllPage(),
       },
       theme: ThemeData(
         primarySwatch: Colors.blue,
